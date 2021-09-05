d537e73fba6f8a5cd087e3d9140d91bd398d9244
Khaled <tetbirtk@gmail.com>
2021-09-05 17:44:20 -0400

    Added alo

--- src/com/compagny/Main.java
+++ src/com/compagny/Main.java
@@ -15,5 +15,7 @@ public class Main {
         System.out.println("mazda has " + mazda.getDoors() + " doors");
         System.out.println("moto has " + moto.getDoors() + " doors");
 
+        System.out.println("alo");
+
     }
 }
