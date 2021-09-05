25686f2f2d3dc70ba0600936cbd96fbd4480aee7
Trapdoge <90121483+Trapdoge@users.noreply.github.com>
2021-09-05 17:33:57 -0400

    addded moto

--- src/com/compagny/Main.java
+++ src/com/compagny/Main.java
@@ -8,11 +8,12 @@ public class Main {
         chevrolet = new Car(4, 4);
         Car mazda;
         mazda = new Car(4, 2);
-
+        Car moto;
+        moto = new Car(2 , 0);
 
         System.out.println("chevrolet has " + chevrolet.getDoors() + " doors");
         System.out.println("mazda has " + mazda.getDoors() + " doors");
-
+        System.out.println("moto has " + moto.getDoors() + " doors");
 
     }
 }
