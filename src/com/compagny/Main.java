381f5568b0e97bddad51c3a3b67e542fa2b0f282
Khaled <tetbirtk@gmail.com>
2021-09-05 17:49:21 -0400

    Added alo

--- src/com/compagny/Main.java
+++ src/com/compagny/Main.java
@@ -19,6 +19,7 @@ public class Main {
         System.out.println("alooooo");
         System.out.println("alooooo");
         System.out.println("alooooo");
+        System.out.println("alooooo");
 
     }
 }
