c81fecaaee94b9903fb52a51fc455655fb8f1e84
Trapdoge <90121483+Trapdoge@users.noreply.github.com>
2021-09-05 17:20:08 -0400

    test 1

---
+++ src/com/compagny/Car.java
@@ -0,0 +1,21 @@
+package com.compagny;
+
+public class Car {
+
+    private int wheels;
+    private int doors;
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public Car(int wheels, int doors) {
+        this.wheels = wheels;
+        this.doors = doors;
+
+    }
+}
---
+++ src/com/compagny/Main.java
@@ -0,0 +1,13 @@
+package com.compagny;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car chevrolet;
+        chevrolet = new Car(4, 4);
+
+        System.out.println("chevrolet has " + chevrolet.getDoors() + " doors");
+
+    }
+}
